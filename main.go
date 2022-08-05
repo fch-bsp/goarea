919e9d133b0edd17bcc3a6f4e79cfefcb8e0ced8
nandohoras <102867453+fch-bsp@users.noreply.github.com>
2022-08-05 08:51:38 -0300

    vRIAÇÃO DO ARQUIVO MAIN.GO

---
+++ main.go
@@ -0,0 +1,29 @@
+package goarea
+
+import "math"
+
+//Pi é uma proporção numérica definida pela relação entre
+// o perímetro de uma cirguferência e seu diâmetro
+
+
+const Pi = 3.1416
+
+// Circ é responsável por calcular a área da circunfêrencia 
+
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+
+}
+
+// Rect é responsável por calcular a área de retangulo
+func Rect(base, altura float64) float64 {
+	return base * altura 
+}
+
+// Não é visível
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+
+}
+
